3259297d4168ae0b32df04a510a68cc144cb695d
Ren <b.b.r.12.11@gmail.com>
2025-04-05 20:21:25 +0900

    Restyled

--- dist/app.js
+++ dist/app.js
@@ -15,12 +15,11 @@ async function getAccessToken() {
     return data.access_token;
 }
 
+const contentsWrapper = document.querySelector("#js-contents-wrapper");
 const album = document.querySelector("#js-album");
 const artist = document.querySelector("#js-artist");
 const imgWrapper = document.querySelector("#js-image-wrapper");
 
-// const fromYear = 1950;
-// const toYear = 1960;
 const selectedDecade = document.querySelector("#decade");
 const button = document.querySelector("#button");
 
@@ -31,6 +30,7 @@ button.addEventListener("click", async () => {
     if (fromYear) {
         document.querySelector("#not-selected").classList.add("hidden");
         imgWrapper.classList.add("max-h-96");
+        imgWrapper.classList.add("mb-3");
         const response = await fetch(
           `https://api.spotify.com/v1/search?q=year:${fromYear}-${toYear}&type=album&limit=50&offset=0`, 
             {
@@ -48,8 +48,10 @@ button.addEventListener("click", async () => {
         const rand = Math.floor(Math.random() * count);
         const albumName = filteredData[rand].name;
         album.innerText = albumName;
+        album.classList.add('mb-1');
         const albumArtist = filteredData[rand].artists[0].name;
         artist.innerText = albumArtist;
+        artist.classList.add('mb-9');
         if (filteredData[rand].images[1].url) {
             const img = document.querySelector("#image");
             img.classList.remove("opacity-0");
@@ -70,91 +72,6 @@ button.addEventListener("click", async () => {
         
     } else {
         document.querySelector("#not-selected").classList.remove("hidden");
-        console.log("error");
-        console.log(fromYear);
+        console.log("Error: Decade not selected");
     }
 });
-
-// async function getAlbum() {
-//     // let allAlbums = [];
-//     // let offset = 0;
-//     const token = await getAccessToken();
-//     const fromYear = Number(selectedDecade.value.slice(0, 4));
-//     const toYear = fromYear + 9;
-//     if (fromYear) {
-//         document.querySelector("#not-selected").classList.add("hidden");
-//         imgWrapper.classList.add("max-h-96");
-//         const response = await fetch(
-//             `https://api.spotify.com/v1/search?q=year:${fromYear}-${toYear}&type=album&limit=50&offset=0`,
-//             {
-//                 headers: { Authorization: `Bearer ${token}` },
-//             }
-//         );
-
-//         const data = await response.json();
-//         const filteredData = data.albums.items.filter((info) => {
-//             return info.album_type === "album";
-//         });
-
-//         const count = filteredData.length;
-//         const rand = Math.floor(Math.random() * count);
-//         const albumName = filteredData[rand].name;
-//         album.innerText = albumName;
-//         const albumArtist = filteredData[rand].artists[0].name;
-//         artist.innerText = albumArtist;
-//         if (filteredData[rand].images[1].url) {
-//             const img = document.querySelector("#image");
-//             img.classList.remove("opacity-0");
-//             img.classList.add("opacity-100");
-//             img.src = filteredData[rand].images[1].url;
-//             // imgWrapper.append(img);
-//         }
-
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//     } else {
-//         document.querySelector("#not-selected").classList.remove("hidden");
-//         console.log("error");
-//         console.log(fromYear);
-//     }
-//     // for(let i=0; i<=100; i += 50) {
-//     //   // offsetをiにすればいいのでは？
-//     // }
-
-//     // const response = await fetch(
-//     //     `https://api.spotify.com/v1/search?q=year:${fromYear}-${toYear}&type=album&limit=50&offset=0`,
-//     //     {
-//     //         headers: { Authorization: `Bearer ${token}` },
-//     //     }
-//     // );
-//     // if (!response.ok) {
-//     //     throw new Error(`HTTP error! Status: ${response.status}`);
-//     // }
-// }
-
-// getAlbum(50);
-// getAlbum(100);
-
-// async function getAlbum(offset) {
-//     const token = await getAccessToken();
-//     const response = await fetch(
-//         // `https://api.spotify.com/v1/search?q=${encodeURIComponent(trackName)}&type=album&limit=50&year:1990-1999`,
-//         `https://api.spotify.com/v1/search?q=year:${fromYear}-${toYear}&type=album&limit=50&offset=${offset}`,
-//         {
-//             headers: { Authorization: `Bearer ${token}` },
-//         }
-//     );
-
-//     const data = await response.json();
-//     console.log(data);
-//     const filteredData = data.albums.items.filter((info) => {
-//         return info.album_type === "album";
-//     });
-//     const count = filteredData.length;
-//     console.log(count);
-//     const rand = Math.floor( Math.random() * count);
-//     console.log(rand);
-//     const albumName = filteredData[rand].name;
-//     console.log(albumName);
-// }
